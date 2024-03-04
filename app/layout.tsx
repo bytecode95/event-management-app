d1a2a31d9d7673c3ebd391092c92d644a49fb67d
chethana95 <chethana.virajini@upview.tech>
2024-03-04 21:36:45 +0530

    authentication with next clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ["latin"],
-  weight : ['400','500','600','700'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-poppins',
 });
 
@@ -22,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
